f61ce123080f270508620f0c81cf6a3539537524
Julian pc-pagano <gregules@pagano-sa.com.ar>
2021-08-06 15:11:02 -0300

    Crea rutas y home.html. Al dia con clase 11

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
 app.get('/', (req, res) => {
-    res.send("Hola Mundo");
+    res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
 app.listen(3000, () => {
