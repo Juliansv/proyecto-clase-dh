cf5a191ad16ab3b7602e7501be94dcab6479d799
Juliansv <juliansv22@gmail.com>
2021-08-18 20:23:01 -0300

    Agrega archivos para deploy en heroku

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en puerto 3000')
 });
